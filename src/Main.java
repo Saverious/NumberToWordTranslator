f460c0255c6113c93605c7d013a05dbb9cdf8699
Saverious <saverious7@gmail.com>
2026-03-30 13:43:40 +0300

    use different test value

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        BigDecimal number = new BigDecimal("0");
+        BigDecimal number = new BigDecimal("82724764624");
         NumericToWord numericToWord = new NumericToWord(number);
         String valueInWords = numericToWord.translate();
 
